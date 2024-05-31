2c5b590fa54072b46518f53fc7fa2cbf9452ee1d
chuannm <chuannm.ctv@gmail.com>
2024-05-31 15:08:58 +0700

    Không cho phép chọn lại câu hỏi

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(() => {
     const $container = $('#game-container');
     const quizData = [
         {
-            id: 1, title:'Quest1', content: "Câu hỏi 1", questions:[
+            title:'Quest 1 title', content: "Câu hỏi 1", questions:[
                 {point: 0, content: "Answer1"}, 
                 {point: 0, content: "Answer2"}, 
                 {point: 0, content: "Answer3"}, 
@@ -12,7 +12,7 @@ $(() => {
             ]
         },
         {
-            id: 1, title:'Quest2', content: "Câu hỏi 2", questions:[
+            title:'Quest 2 title', content: "Câu hỏi 2", questions:[
                 {point: 0, content: "Answer1"}, 
                 {point: 0, content: "Answer2"}, 
                 {point: 0, content: "Answer3"}, 
@@ -27,23 +27,28 @@ $(() => {
     }
     function addItem(itemData) {
         if (!itemData) return;
-        const $item = $(`<div id="${itemData.id}" class="question-container ui-widget ui-corner-all"></div>`)
+        const $item = $(`<div class="question-container ui-widget ui-corner-all"></div>`)
         $item.append(`<div class="title">${itemData.title}</div>`)
         $item.append(`<div class="content">${itemData.content}</div>`)
-        const $questionContainer = $(`<div class="answers-container ui-widget ui-corner-all"></div>`)
-        $item.append($questionContainer);
+        const $answersContainer = $(`<div class="answers-container ui-widget ui-corner-all"></div>`)
+        $item.append($answersContainer);
         $item.markDone = false;
         for(let a of itemData.questions) {
             const $btn = $(`<button class="ui-widget ui-corner-all ui-button">${a.content}</button>`)
             $btn.button();
             $btn.click(() => {
                 addPoint(a.point)
-                $btn.append(`<span style="font-size: 12px; color: Dodgerblue;"><i class="fa-solid fa-check"></i>+${a.point}</span>`)
-                $btn.off('click');
+                if (a.point > 0) {
+                    $btn.append(`<span style="font-size: 12px; color: Dodgerblue;"><i class="fa-solid fa-check"></i></span>`)    
+                } else {
+                    $btn.append(`<span style="font-size: 12px; color: red;"><i class="fa-solid fa-xmark"></i>+${a.point}</span>`)    
+                }
+                $('button', $item).off('click').button('option', 'disabled', true);
                 $btn.addClass("answered")
             })
-            $item.append($btn)
+            $answersContainer.append($btn)
         }
+
         $container.append($item)
     }
 
