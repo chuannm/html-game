a2a738367b254e69beb0656069d4fa499536bb2f
chuannm <chuannm.ctv@gmail.com>
2024-06-11 09:27:03 +0700

    Thêm cột last_question_index

--- api/common/ddl.sql
+++ api/common/ddl.sql
@@ -4,6 +4,7 @@ name VARCHAR(200) NOT NULL,
 json_data JSON,
 high_score integer DEFAULT 0,
 last_answer_is_correct integer DEFAULT 0,
+last_question_index integer DEFAULT -1,
 update_time TIMESTAMP DEFAULT NOW());
 
 ALTER TABLE "user_data" ADD IF NOT EXISTS last_answer_is_correct integer DEFAULT 0; 
