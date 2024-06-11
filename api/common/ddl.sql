6025245dd04fab5786cad3112cad8e5e9b1a50f0
chuannm <chuannm.ctv@gmail.com>
2024-06-11 09:26:33 +0700

    Thêm cột last_question_index

--- api/common/ddl.sql
+++ api/common/ddl.sql
@@ -7,3 +7,4 @@ last_answer_is_correct integer DEFAULT 0,
 update_time TIMESTAMP DEFAULT NOW());
 
 ALTER TABLE "user_data" ADD IF NOT EXISTS last_answer_is_correct integer DEFAULT 0; 
+ALTER TABLE "user_data" ADD IF NOT EXISTS last_question_index integer DEFAULT -1; 
