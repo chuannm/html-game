7092d986f3c51897dbb1ea2e4de1c31021e6858b
chuannm <chuannm.ctv@gmail.com>
2024-06-14 09:11:45 +0700

    Thêm trường start_time để biết thời điểm bắt đầu trả lời câu hỏi.

--- api/common/ddl.sql
+++ api/common/ddl.sql
@@ -5,7 +5,9 @@ json_data JSON,
 high_score integer DEFAULT 0,
 last_answer_is_correct integer DEFAULT 0,
 last_question_index integer DEFAULT -1,
+start_time TIMESTAMP DEFAULT NOW(),
 update_time TIMESTAMP DEFAULT NOW());
 
 ALTER TABLE "user_data" ADD IF NOT EXISTS last_answer_is_correct integer DEFAULT 0; 
 ALTER TABLE "user_data" ADD IF NOT EXISTS last_question_index integer DEFAULT -1; 
+ALTER TABLE "user_data" ADD IF NOT EXISTS start_time TIMESTAMP DEFAULT NOW(); 
--- api/index.py
+++ api/index.py
@@ -1,4 +1,3 @@
-import datetime
 import os, json
 from time import time
 from math import floor
@@ -8,71 +7,52 @@ from .common.db import executeSQL, closeDB
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.secret_key = "$3CR3TKE1"
-
-
 @app.route('/')
 def home():
     return render_template("index.html")
-
-
+    
 @app.route('/api/get-quiz/')
 def readQuizJson():
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "quiz-data.json")
     with open(path, "r") as json_file: return json.loads(json_file.read())
 
-
-@app.route('/api/user_data/<user_id>', methods=["GET", "POST"])
+@app.route('/api/user_data/<user_id>', methods = ["GET", "POST"])
 def user_data(user_id):
-    if not user_id: return {"code": "400", "data": "BAD_REQUEST"}
+    if not user_id: return { "code": "400", "data": "BAD_REQUEST"}
     if request.method == 'POST':
         data = request.get_json(True)
-        print(data)
-
         answered_data = data["answered_data"]
-        last_point = data["lastPoint"]
         name = data["name"]
         high_score = get_total_score(answered_data)
-        last_question_index = data["last_question_index"]
-        last_answer_is_correct = 1 if last_point > 0 else 0
-        print("last_answer_is_correct: ", last_answer_is_correct)
-        sql = """INSERT INTO user_data (id, name, json_data, high_score, last_answer_is_correct, last_question_index)
-        VALUES (%s, %s, %s, %s, %s ,%s) 
+        sql = """INSERT INTO user_data (id, name, json_data, high_score) VALUES (%s, %s, %s, %s) 
             ON CONFLICT (id) DO UPDATE 
             SET 
                 json_data = EXCLUDED.json_data, 
                 high_score = GREATEST(EXCLUDED.high_score, (SELECT high_score FROM user_data WHERE id = EXCLUDED.id)), 
-                last_answer_is_correct = EXCLUDED.last_answer_is_correct,
-                last_question_index = EXCLUDED.last_question_index,
                 update_time = NOW()
         """
         try:
-            cursor = executeSQL(sql, [user_id, name, json.dumps(answered_data), high_score, last_answer_is_correct, last_question_index])
-        except Exception as e:
-            print(e)
-            return {"code": "500", "data": "DATA ERROR"}
+            cursor = executeSQL(sql, [user_id, name, json.dumps(answered_data), high_score])
+        except Exception as e: 
+            print (e)
+            return { "code": "500", "data": "DATA ERROR"} 
 
-    sql = "SELECT json_data::json, extract(epoch from update_time), high_score, last_answer_is_correct," \
-          " last_question_index, extract(epoch from NOW()) FROM user_data WHERE id = %s"
+    sql = "SELECT json_data::json, extract(epoch from update_time), high_score, extract(epoch from NOW()) FROM user_data WHERE id = %s"
     try:
-        cursor = executeSQL(sql, [user_id, ])
+        cursor = executeSQL(sql, [user_id,])
         row = cursor.fetchone()
-        return {"data": None} if not row else {
-            "data": {"answered": row[0], "time": row[1], "high_score": row[2], "last_answer_is_correct": row[3],
-                     "last_question_index": row[4], "now": row[5]}}
+        return {"data": None} if not row else {"data": { "answered": row[0], "time": row[1], "high_score": row[2], "now": row[3] } }
     except Exception as e:
-        print(e)
-        return {"code": "400", "data": "BAD_REQUEST"}
+        print (e)
+        return { "code": "400", "data": "BAD_REQUEST"}
     finally:
         closeDB(True)
-
-
 @app.route('/api/ranks')
-def getRanks():
-    sql = "SELECT id, name, extract(epoch from update_time) * 1000, high_score FROM user_data ORDER BY high_score DESC, update_time DESC LIMIT 100"
-    cursor = executeSQL(sql, [])
-    records = [{"id": row[0], "name": row[1], "timed": int(row[2]), "score": int(row[3])} for row in cursor.fetchall()]
-    return {"data": records}
-
+def getRanks() :
+        sql = "SELECT id, name, extract(epoch from update_time) * 1000, high_score FROM user_data ORDER BY high_score DESC, update_time DESC LIMIT 100"
+        cursor = executeSQL(sql, [])
+        records = [{"id": row[0], "name": row[1], "timed": int(row[2]), "score": int(row[3])} for row in cursor.fetchall()]
+        return {"data": records}
 
 def get_total_score(answered_data):
     score = 0
@@ -80,8 +60,3 @@ def get_total_score(answered_data):
         a = answered_data[key]
         score += a['point']
     return score
-
-
-def get_last_answer_is_correct(answered_data):
-    # return 1 if last answer point > 0 else 0
-    return 1 if answered_data[list(answered_data.keys())[-1]]['point'] > 0 else 0
